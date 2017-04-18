fce321292bf1559ed7541909e07095e3c5c011bc
Horvath Balazs <balazs.horvath989@gmail.com>
2017-04-18 09:48:23 +0200

    new test added for only odd numbers

--- oddavg/odd_avg_test.py
+++ oddavg/odd_avg_test.py
@@ -8,3 +8,6 @@ class TestOddAverage(unittest.TestCase):
 
     def test_odd_average_numbers_1_to_5(self):
         self.assertEqual(odd_avg.odd_average([1, 2, 3, 4, 5]), 3)
+
+    def test_odd_average_only_odd_numbers(self):
+        self.assertEqual(odd_avg.odd_average([1, 5, 3, 7, 9]), 5)
