766aff05e2fc396e1518438aab1dc29a8a4a26e3
Horvath Balazs <balazs.horvath989@gmail.com>
2017-04-18 11:41:18 +0200

    read_write method created, opens one files content and writes it into a new file

--- copy/copy.py
+++ copy/copy.py
@@ -15,11 +15,24 @@ class Copy(object):
     def __init__(self):
         self.args = sys.argv
         if len(self.args) == 1:
-            print("Enter after the file name: copy [source] [destination]")
+            print("Enter after the file name: copy 'source' 'destination'")
         else:
             self.command = sys.argv[1:]
             if len(self.command) < 2:
                 print('No destination provided')
+            if len(self.command) == 3 and self.command[0] == 'copy':
+                self.read_write(self.command[1], self.command[2])
+
+    def read_write(self, source, destination):
+        try:
+            source_file = open(source, mode='r')
+            file_content = source_file.read()
+            source_file.close()
+            new_file = open(destination, mode='w')
+            new_file.write(file_content)
+            new_file.close()
+        except FileNotFoundError:
+            print('File not found or not exists')
 
 
 copy = Copy()
